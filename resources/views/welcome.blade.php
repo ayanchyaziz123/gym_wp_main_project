ff20d14b0c3571bae54eca04c99e336f49b3e6c4
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2021-01-27 00:20:47 +0600

    add modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -145,28 +145,85 @@
             <a class="nav-link active bg-info" href="{{route('welcome')}}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link"  data-toggle="modal" data-target=".bd-example-modal-lgg" href="">Location</a>
+            <a class="nav-link" data-toggle="modal" data-target=".bd-example-modal-lgg" href="">Location</a>
           </li>
           <!-- The Modal -->
           <div class="modal fade bd-example-modal-lgg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
               <div class="modal-content p-5">
-              <h4>Location</h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis aliquam rerum consequatur, cumque, 
-                consectetur, maiores in hic accusamus molestias beatae a temporibus totam velit magni magnam praesentium officiis non? Quibusdam.
+                <div class="row row-content">
+                  <div class="col-12">
+                    <h3>Location Information</h3>
+                  </div>
+                  <div class="col-12 col-sm-4 offset-sm-1">
+                    <h5>Our Address</h5>
+                    <address style="font-size: 100%">
+                      121, Clear Water Bay Road<br>
+                      Clear Water Bay, Kowloon<br>
+                      HONG KONG<br>
+                      <i class="fa fa-phone"></i>: +852 1234 5678<br>
+                      <i class="fa fa-fax"></i>: +852 8765 4321<br>
+                      <i class="fa fa-envelope"></i>:
+                      <a href="mailto:confusion@food.net">confusion@food.net</a>
+                    </address>
+                  </div>
+                  <div class="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                  </div>
+                  <div class="col-12 col-sm-11 offset-sm-1">
+                    <div class="btn-group" role="group">
+                      <a role="button" class="btn btn-primary" href="tel:+85212345678"><i class="fa fa-phone"></i> Call</a>
+                      <a role="button" class="btn btn-info"><i class="fa fa-skype"></i> Skype</a>
+                      <a role="button" class="btn btn-success" href="mailto:confusion@food.net"><i class="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <li class="nav-item">
-            <a class="nav-link"  data-toggle="modal" data-target=".bd-example-modal-lg" href="">About Us</a>
+            <a class="nav-link" data-toggle="modal" data-target=".bd-example-modal-lg" href="">About Us</a>
           </li>
           <!-- The Modal -->
           <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
               <div class="modal-content p-5">
-              <h4>Our Gym</h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis aliquam rerum consequatur, cumque, 
-                consectetur, maiores in hic accusamus molestias beatae a temporibus totam velit magni magnam praesentium officiis non? Quibusdam.
+              <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-6">
+                <h2>Our History</h2>
+                <p>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
+                <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
+            </div>
+                       <div class="col-12 col-sm-6">
+                <div class="card">
+                    <h3 class="card-header bg-primary text-white">Facts At a Glance</h3>
+                    <div class="card-body">
+                        <dl class="row">
+                            <dt class="col-6">Started</dt>
+                            <dd class="col-6">3 Feb. 2013</dd>
+                            <dt class="col-6">Major Stake Holder</dt>
+                            <dd class="col-6">HK Fine Foods Inc.</dd>
+                            <dt class="col-6">Last Year's Turnover</dt>
+                            <dd class="col-6">$1,250,375</dd>
+                            <dt class="col-6">Employees</dt>
+                            <dd class="col-6">40</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+             <div class="col-12">
+                <div class="card card-body bg-light">
+                    <blockquote class="blockquote">
+                        <p class="mb-0">You better cut the pizza in four pieces because
+                            I'm not hungry enough to eat six.</p>
+                        <footer class="blockquote-footer">Yogi Berra,
+                            <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
+                            P. Pepe, Diversion Books, 2014</cite>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
               </div>
             </div>
           </div>
